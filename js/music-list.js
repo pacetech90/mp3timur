7c60a0955e585f8f1963a87773818496cefb7191
PACE TECH <55420029+pacetech90@users.noreply.github.com>
2024-02-05 15:11:58 +0900

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,22 @@
+// masukan semua list musik disini
+
+let allMusic = [
+  {
+    name: "Darah Tinggi - Toton Caribo",
+    artist: "Toton Caribo",
+    img: "toton",
+    src: "lagu-3"
+  },
+  {
+    name: "Justy Aldrin - Luka Kanapa",
+    artist: "Justy Aldrin",
+    img: "justy",
+    src: "lagu-2"
+  },
+  {
+    name: "Wizz Baker - Malam",
+    artist: "Wizz Baker",
+    img: "wizz",
+    src: "lagu-1"
+  }
+ ];
